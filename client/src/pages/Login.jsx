d7cd0f04a53dd112bcaefa8878ad3ea5c0407be7
akashroy8210 <akash.roy@iitg.ac.in>
2026-07-17 11:17:39 +0530

    Add useEffect import to Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../services/api.service';
 import { Key, User, ShieldAlert, Heart, Globe, AlertCircle, ArrowRight, Eye, EyeOff } from 'lucide-react';
